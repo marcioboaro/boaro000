544fbc34473bdaff4abc0d28af4d31d40e39f3f7
Márcio Boaro <marcio.boaro@gmail.com>
2018-01-06 22:59:32 -0200

    boaro000 - correção

--- boaro000/settings.py
+++ boaro000/settings.py
@@ -1,5 +1,5 @@
 import os
-#import dj_database_url
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -110,7 +110,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES('default').update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
